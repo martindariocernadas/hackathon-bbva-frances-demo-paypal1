77cc5f331f27830c42bc67c3620a0a99cbf1bb41
Martin Dario Cernadas <martindario.cernadas@bbva.com>
2017-10-09 16:41:29 -0300

    Initial

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# hackathon-bbva-frances-example1
+#hackathon ejemplo cliente API para Paypal
+
+- https://github.com/paypal/PayPal-Python-SDK
+
+- Dashboard de la Sandbox : https://developer.paypal.com/developer/dashboard/sandbox/
+
+- Codigo Python para crear un pago exitosamente con las credenciales activas : create-payment.py
+
---
+++ create-payment.py
@@ -0,0 +1,32 @@
+import paypalrestsdk
+import logging
+
+paypalrestsdk.configure({
+  "mode": "sandbox", # sandbox or live
+  "client_id": "Aei_eyReR0PM8caNOXAQb652HWiLrFSHkzvkkScdd2jhPcRQhlWtwpCbmrYoD099PRRnKfr_ZQGNt-XB",
+  "client_secret": "EPd7-vTl7rFqZWf2oS7X8xAVHlU14HziCGZLRY6-ujVUc_gfJ8dXDmDXb5jD9gdiAz67AApz3RErrT9Q" })
+
+payment = paypalrestsdk.Payment({
+    "intent": "sale",
+    "payer": {
+        "payment_method": "paypal"},
+    "redirect_urls": {
+        "return_url": "http://localhost:3000/payment/execute",
+        "cancel_url": "http://localhost:3000/"},
+    "transactions": [{
+        "item_list": {
+            "items": [{
+                "name": "item",
+                "sku": "item",
+                "price": "1.00",
+                "currency": "USD",
+                "quantity": 1}]},
+        "amount": {
+            "total": "1.00",
+            "currency": "USD"},
+        "description": "Esta es una transaccion de pago de ejemplo."}]})
+
+if payment.create():
+  print("Pago creado con exito")
+else:
+  print(payment.error)
